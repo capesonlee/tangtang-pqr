ba09645d935e35d4336f7b5863c3ac66cfe1765a
lijuyong <lijuyong@youbangsoft.com>
2017-09-07 21:13:42 +0800

    第一个版本

---
+++ MeixingApplication/MeixingApplication/main.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace MeixingApplication
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblModel = new System.Windows.Forms.Label();
+            this.lblCode = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblPeriod = new System.Windows.Forms.Label();
+            this.txtPeriod = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.printPreviewControl = new System.Windows.Forms.PrintPreviewControl();
+            this.docToPrint = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(44, 26);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 12);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "名称：";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblModel
+            // 
+            this.lblModel.AutoSize = true;
+            this.lblModel.Location = new System.Drawing.Point(44, 58);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(41, 12);
+            this.lblModel.TabIndex = 1;
+            this.lblModel.Text = "型号：";
+            // 
+            // lblCode
+            // 
+            this.lblCode.AutoSize = true;
+            this.lblCode.Location = new System.Drawing.Point(297, 61);
+            this.lblCode.Name = "lblCode";
+            this.lblCode.Size = new System.Drawing.Size(41, 12);
+            this.lblCode.TabIndex = 2;
+            this.lblCode.Text = "编号：";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(20, 96);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(65, 12);
+            this.lblAddress.TabIndex = 3;
+            this.lblAddress.Text = "使用地点：";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(91, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(192, 21);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(92, 58);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(191, 21);
+            this.txtModel.TabIndex = 5;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(334, 58);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(155, 21);
+            this.txtCode.TabIndex = 6;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(91, 96);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(397, 21);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // lblPeriod
+            // 
+            this.lblPeriod.AutoSize = true;
+            this.lblPeriod.Location = new System.Drawing.Point(32, 136);
+            this.lblPeriod.Name = "lblPeriod";
+            this.lblPeriod.Size = new System.Drawing.Size(53, 12);
+            this.lblPeriod.TabIndex = 8;
+            this.lblPeriod.Text = "有效期：";
+            // 
+            // txtPeriod
+            // 
+            this.txtPeriod.Location = new System.Drawing.Point(91, 133);
+            this.txtPeriod.Name = "txtPeriod";
+            this.txtPeriod.Size = new System.Drawing.Size(397, 21);
+            this.txtPeriod.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(491, 162);
+            this.panel1.TabIndex = 10;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(567, 154);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(89, 24);
+            this.btnPrint.TabIndex = 11;
+            this.btnPrint.Text = "打印";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(716, 155);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(75, 23);
+            this.btnPreview.TabIndex = 12;
+            this.btnPreview.Text = "预览";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // printPreviewControl
+            // 
+            this.printPreviewControl.Location = new System.Drawing.Point(12, 193);
+            this.printPreviewControl.Name = "printPreviewControl";
+            this.printPreviewControl.Size = new System.Drawing.Size(521, 235);
+            this.printPreviewControl.TabIndex = 13;
+            // 
+            // docToPrint
+            // 
+            this.docToPrint.DocumentName = "docToPrint";
+            this.docToPrint.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.docToPrint_PrintPage);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 440);
+            this.Controls.Add(this.printPreviewControl);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.txtPeriod);
+            this.Controls.Add(this.lblPeriod);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblCode);
+            this.Controls.Add(this.lblModel);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "main";
+            this.Text = "条码打印";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblModel;
+        private System.Windows.Forms.Label lblCode;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblPeriod;
+        private System.Windows.Forms.TextBox txtPeriod;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.PrintPreviewControl printPreviewControl;
+        private System.Drawing.Printing.PrintDocument docToPrint;
+    }
+}
+
---
+++ MeixingApplication/MeixingApplication/main.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThoughtWorks.QRCode.Codec;
+
+namespace MeixingApplication
+{
+    public partial class main : Form
+    {
+        static protected double InchToMm= 0.0393701 * 100;
+        protected int PRINT_WIDTH = Convert.ToInt32(75 * InchToMm);
+        protected int PRINT_HEIGHT = Convert.ToInt32(50 * InchToMm);
+        public main()
+        {
+            InitializeComponent();
+            
+            int width = PRINT_WIDTH;
+            int height = PRINT_HEIGHT;
+            //this.docToPrint.DefaultPageSettings.Margins = new Margins(margin, margin, margin, margin, margin);
+            this.docToPrint.DefaultPageSettings.PaperSize = new PaperSize("Custum", width, height);
+            
+            this.txtName.Text = "电烤箱";
+            this.txtModel.Text = "ST--120B";
+            this.txtCode.Text = "K-045";
+            this.txtAddress.Text = "上海一车间在用";
+            this.txtPeriod.Text = "2015年12月31日到2016年12月31日";
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+
+            this.printPreviewControl.Document = this.docToPrint;
+           
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+
+            PrintDocument pdoc = new PrintDocument();
+            pdoc.PrintPage += new PrintPageEventHandler(docToPrint_PrintPage);
+            pdoc.Print();
+
+            //this.docToPrint += new PrintPageEventHandler(docToPrint_PrintPage);
+        }
+
+        private void docToPrint_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+            Font fontLarge = new Font("仿宋", 18, FontStyle.Bold);
+            Font fontMiddle = new Font("仿宋", 12, FontStyle.Bold);
+            Font fontSmall = new Font("仿宋", 10, FontStyle.Regular);
+            Brush bru = Brushes.Black;
+            Graphics g = e.Graphics;   //先建立画布 
+            String companyName = "上海美星电子有限公司";
+            
+            int x = 10;
+            int y = 5;
+           
+            g.DrawString(companyName, fontLarge, bru, x, y);
+
+            /*--------------------------------------------------------*/
+            x = 150;
+            y += 30;
+            g.DrawString(this.lblName.Text,fontMiddle,bru, x, y);
+         
+            x += 40;
+            g.DrawString(this.txtName.Text, fontMiddle, bru, x, y);
+
+            /*--------------------------------------------------------*/
+            x = 150;
+            y += 30;
+            g.DrawString(this.lblModel.Text, fontMiddle, bru, x, y);
+            x += 40;
+            g.DrawString(this.txtModel.Text, fontMiddle, bru, x, y);
+
+            /*--------------------------------------------------------*/
+            x = 150;
+            y += 30;
+            g.DrawString(this.lblCode.Text, fontMiddle, bru, x, y);
+            x += 40;
+            g.DrawString(this.txtCode.Text, fontMiddle, bru, x, y);
+
+            /*--------------------------------------------------------*/
+            x = 150;
+            y += 30;
+            g.DrawString(this.lblAddress.Text, fontMiddle, bru, x, y);
+            //x += 60;
+            y += 25;
+            g.DrawString(this.txtAddress.Text, fontMiddle, bru, x, y);
+
+            /*--------------------------------------------------------*/
+            x = 10;
+            y += 30;
+            g.DrawString(this.lblPeriod.Text, fontSmall, bru, x, y);
+            x += 55;
+            g.DrawString(this.txtPeriod.Text, fontSmall, bru, x, y);
+
+            Bitmap bt;
+
+            string enCodeString = this.txtPeriod.Text;
+
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+
+            bt = qrCodeEncoder.Encode(enCodeString, Encoding.UTF8);
+
+          
+            Pen pen = new Pen(bru,1);
+            Rectangle test = new Rectangle(0, 0, PRINT_WIDTH, PRINT_HEIGHT);
+            g.DrawRectangle(pen, test);
+            g.DrawImage(bt,15,35,135,135);
+          
+        }
+
+     
+
+       
+    }
+}
